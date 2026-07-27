c35289b9e8c93758ff69101805907d0871f3f6ed
Pranav Sikhwal <pranavsikhwal@gmail.com>
2026-07-27 10:52:02 +0530

    refactor: restructure redis_listener function for improved readability

--- backend/redis_listener.py
+++ backend/redis_listener.py
@@ -4,15 +4,21 @@ from redis_client import redis_client
 from connection_manager import manager, CHANNEL_NAME
 
 async def redis_listener():   #creating a function that keeps listening forever 
-    pubsub = redis_client.pubsub()  #creates the subscriber part 
-    await pubsub.subscribe(CHANNEL_NAME)  
-    print(f"Subscribed to Redis channel: {CHANNEL_NAME}")
+   while True:
+        try:
+            pubsub = redis_client.pubsub()
+            await pubsub.subscribe(CHANNEL_NAME)
+            print(f"Subscribed to Redis channel: {CHANNEL_NAME}")
 
-    async for message in pubsub.listen():
-        if message["type"] == "message":
-            data = json.loads(message["data"])
-            user_id = data["user_id"]
-            notification_payload = data["message"]
-            await manager.send_notification(user_id, notification_payload)
+            async for message in pubsub.listen():
+                if message["type"] == "message":
+                    data = json.loads(message["data"])
+                    user_id = data["user_id"]
+                    notification_payload = data["message"]
+                    await manager.send_notification(user_id, notification_payload)
+
+        except Exception as e:
+            print(f"Redis listener crashed: {e}. Reconnecting in 5 seconds...")
+            await asyncio.sleep(5)
             
           #dumps need to convert Python to json and loads does the opposite work 
